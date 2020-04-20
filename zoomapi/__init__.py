a841e9e1cb39f787d7655b7b79a8a7abc4e355ba
Soobin Choi <soobinchoi54@gmail.com>
2020-04-20 03:35:11 -0700

    delete

--- zoomapi/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Python wrapper around the Zoom.us REST API"""
-
-from zoomapi.jwtclient import JWTZoomClient
-from zoomapi.oauthclient import OAuthZoomClient
-from zoomapi.client import ZoomClient
-
-__all__ = ["ZoomClient", "JWTZoomClient", "OAuthZoomClient"]
-__version__ = "1.1.1"
